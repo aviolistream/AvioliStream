db7a87f6038e7815b7f0994398f5171a80ee8f9d
aviolistream <aviolistream@gmail.com>
2026-09-01 07:18:41 +0100

    Delete src directory

--- src/routes/subscriptionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { requireAuth } = require('../middleware/auth');
-const { createCheckoutSession } = require('../controllers/subscriptionController');
-
-router.post('/checkout', requireAuth, createCheckoutSession);
-
-module.exports = router;
